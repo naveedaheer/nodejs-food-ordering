f527e33d2b52fa18bf26a2f0a9bc3f0c70662a6d
Naveed Aheer <naveedaheerpc@gmail.com>
2022-12-11 03:59:27 +0500

    updated

--- src/order-book/controller.js
+++ src/order-book/controller.js
@@ -4,33 +4,33 @@ const orderBookService = require("./services");
 
 const foodItems = [
   {
-    id: '1',
+    id: 1,
     name: "Sandwich",
     imageUrl: 'https://hamariweb.com/recipes/images/recipeimages/483.jpg',
     price: 10,
     availableQty: 95,
     description: "A sandwich is a food typically consisting of vegetables, sliced cheese or meat, placed on or between slices of bread, or more generally any dish wherein bread serves as a container or wrapper for another food type.",
 
-    id: '2',
+    id: 2,
     name: "Burger King",
     imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6zFRuYZrR7fObTrM7pRszE0D67CoF84YVnA&usqp=CAU',
     price: 50,
     availableQty: 35,
     description: "Now serving all your favourite burgers in soft-n-fresh potato bun. The buns are specially selected to enhance the flavour of each bite.",
 
-    id: '3',
+    id: 3,
     name: "Chicken Tikka",
     imageUrl: 'https://www.charbroil.com/media/ctm//T/a/Tandoori-Style_Grilled_Chicken_Tikka_Masala.jpg.jpeg',
     price: 10,
     availableQty: 95,
     description: "How to make chicken tikka kebabs · Place the chicken pieces into a bowl with yogurt, garlic, ginger, lemon juice, coriander, turmeric, cumin.",
 
-    id: '1',
-    name: "sandwich",
-    imageUrl: '',
+    id: 4,
+    name: "Pizza",
+    imageUrl: 'https://foodnerd.s3.eu-west-1.amazonaws.com/production/image/file/293/Jalal___sons_Lahore.png',
     price: 10,
     availableQty: 95,
-    description: "A sandwich is a food typically consisting of vegetables, sliced cheese or meat, placed on or between slices of bread, or more generally any dish wherein bread serves as a container or wrapper for another food type.",
+    description: "Pizza hut is going through a transition phase at the moment. we will be back soon with better quality and service. We Apologize for the inconvenience.",
 
     id: '1',
     name: "sandwich",
