019c65035cfde3e62b241eea9bd56ca4f7f32519
Naveed Aheer <naveedaheerpc@gmail.com>
2022-12-11 04:05:12 +0500

    updated

--- src/order-book/controller.js
+++ src/order-book/controller.js
@@ -9,42 +9,40 @@ const foodItems = [
     imageUrl: 'https://hamariweb.com/recipes/images/recipeimages/483.jpg',
     price: 10,
     availableQty: 95,
-    description: "A sandwich is a food typically consisting of vegetables, sliced cheese or meat, placed on or between slices of bread, or more generally any dish wherein bread serves as a container or wrapper for another food type.",
-
+    description: "A sandwich is a food typically consisting of vegetables, sliced cheese or meat, placed on or between slices of bread, or more generally any dish wherein bread serves as a container or wrapper for another food type."
+  },
+  {
     id: 2,
     name: "Burger King",
     imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6zFRuYZrR7fObTrM7pRszE0D67CoF84YVnA&usqp=CAU',
     price: 50,
     availableQty: 35,
     description: "Now serving all your favourite burgers in soft-n-fresh potato bun. The buns are specially selected to enhance the flavour of each bite.",
-
+  },
+  {
     id: 3,
     name: "Chicken Tikka",
     imageUrl: 'https://www.charbroil.com/media/ctm//T/a/Tandoori-Style_Grilled_Chicken_Tikka_Masala.jpg.jpeg',
     price: 10,
-    availableQty: 95,
+    availableQty: 50,
     description: "How to make chicken tikka kebabs · Place the chicken pieces into a bowl with yogurt, garlic, ginger, lemon juice, coriander, turmeric, cumin.",
-
+  },
+  {
     id: 4,
     name: "Pizza",
     imageUrl: 'https://foodnerd.s3.eu-west-1.amazonaws.com/production/image/file/293/Jalal___sons_Lahore.png',
     price: 10,
-    availableQty: 95,
+    availableQty: 33,
     description: "Pizza hut is going through a transition phase at the moment. we will be back soon with better quality and service. We Apologize for the inconvenience.",
+  },
 
-    id: '1',
-    name: "sandwich",
-    imageUrl: '',
+  {
+    id: 5,
+    name: "Soup",
+    imageUrl: 'https://www.allrecipes.com/thmb/pTGS0SZsSQK85sV_RQE_K6ZfoN4=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/26460-quick-and-easy-chicken-noodle-soup-allrecipes-1x1-1-b88125437574471db3e114c40bc6928e.jpg',
     price: 10,
     availableQty: 95,
-    description: "A sandwich is a food typically consisting of vegetables, sliced cheese or meat, placed on or between slices of bread, or more generally any dish wherein bread serves as a container or wrapper for another food type.",
-
-    id: '1',
-    name: "sandwich",
-    imageUrl: '',
-    price: 10,
-    availableQty: 95,
-    description: "A sandwich is a food typically consisting of vegetables, sliced cheese or meat, placed on or between slices of bread, or more generally any dish wherein bread serves as a container or wrapper for another food type."
+    description: "This easy chicken noodle soup recipe made with veggies and leftover chicken is warm, comforting.",
   }
 ]
 
