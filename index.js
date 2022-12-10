11ec0990712383e638c59195b1ee8a89cd85416a
Naveed Aheer <naveedaheerpc@gmail.com>
2022-12-11 03:29:23 +0500

    updated

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ app.use(express.json());
 app.use(cors());
 app.use("/order-book", orderBookController);
 
-const PORT = 3000;
-app.listen(process.env.PORT || 3000, () => console.log(`Lisening on port ${PORT}`));
+const PORT = 3001;
+app.listen(process.env.PORT || 3001, () => console.log(`Lisening on port ${PORT}`));
