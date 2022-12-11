10a10b5d56afcb5e818f681f262665cebaf916ba
Naveed Aheer <naveedaheerpc@gmail.com>
2022-12-11 05:09:48 +0500

    Updated descriptions

--- src/order-book/controller.js
+++ src/order-book/controller.js
@@ -9,7 +9,7 @@ const foodItems = [
     imageUrl: 'https://static.toiimg.com/photo/92494266.cms',
     price: 10,
     availableQty: 95,
-    description: "A sandwich is a food typically consisting of vegetables, sliced cheese or meat, placed on or between slices of bread, or more generally any dish wherein bread serves as a container or wrapper for another food type."
+    description: "A sandwich is a food typically consisting of vegetables, sliced cheese or meat, placed on or between slices of bread."
   },
   {
     id: 2,
@@ -25,7 +25,7 @@ const foodItems = [
     imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Tandoori_chicken_laccha_piyaz1_%2836886283595%29.jpg/1200px-Tandoori_chicken_laccha_piyaz1_%2836886283595%29.jpg',
     price: 10,
     availableQty: 0,
-    description: "How to make chicken tikka kebabs · Place the chicken pieces into a bowl with yogurt, garlic, ginger, lemon juice, coriander, turmeric, cumin.",
+    description: "How to make chicken tikka kebabs · Place the chicken pieces into a bowl with yogurt, garlic, ginger.",
   },
   {
     id: 4,
@@ -33,7 +33,7 @@ const foodItems = [
     imageUrl: 'https://static.toiimg.com/photo/msid-87930581/87930581.jpg',
     price: 10,
     availableQty: 33,
-    description: "Pizza hut is going through a transition phase at the moment. we will be back soon with better quality and service. We Apologize for the inconvenience.",
+    description: "Pizza hut is going through a transition phase at the moment. we will be back soon with better quality and service.",
   },
 
   {
